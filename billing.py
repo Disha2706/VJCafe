37411bec247074c15178d55bb44bc896f0d4da24
DishaLogs <ldisha2000@gmail.com>
2019-11-19 23:04:40 +0530

    edit before formatting

--- billing.py
+++ billing.py
@@ -33,22 +33,7 @@ lblinfo = Label(tops,font = ('ariel',50,'bold', ), text = "Restaurant Management
 lblinfo.grid(row=0,column=0)
 lblinfo = Label(tops,font = ('ariel',20,'bold', ), text = localtime,fg = "blue", bd = 10, anchor = 'w')
 lblinfo.grid(row=1,column=0)
-#---------calculator----------#
-def btnClick(numbers):
-    global operator
-    operator = operator + str(numbers)
-    text_Input.set(operator)
 
-def btnClear():
-    global operator
-    operator = ""
-    text_Input.set("")
-
-def btnEquals():
-    global operator
-    sumUp = str(eval(operator))
-    text_Input.set(sumUp)
-    operator = ""
 #................buttons.................
 def Ref():
     x = random.randint(200000,700000)
@@ -96,108 +81,91 @@ def Reset():
     Tax.set("")
     TotalCost.set("")
 
+"""def price():
+    def Login_Window(self):
+        self.SaleWindow=Toplevel(self.master)
+        self.app=Billing(self.SaleWindow)"""
+
  #..............................................................................
 
-txtDisplay = Entry(rf,font = ('arial',20,'bold' ),textvariable = text_Input, insertwidth = 4, bd = 20, bg = "light pink", justify = "right")
-txtDisplay.grid(columnspan = 4)
 
-
-btn1 =  Button(rf,padx=16,pady=16,bd = 8,bg = 'light pink',font = ('arial',20,'bold'), text = "1", command = lambda:btnClick(1)).grid(row=2,column=0)
-btn2 =  Button(rf,padx=16,pady=16,bd = 8,bg = 'light pink',font = ('arial',20,'bold'), text = "2", command = lambda:btnClick(2)).grid(row=2,column=1)
-btn3 =  Button(rf,padx=16,pady=16,bd = 8,bg = 'light pink',font = ('arial',20,'bold'), text = "3", command = lambda:btnClick(3)).grid(row=2,column=2)
-Addition =  Button(rf,padx=16,pady=16,bd = 8,bg = 'light pink',font = ('arial',20,'bold'), text = "+", command = lambda:btnClick("+")).grid(row=2,column=3)
-
-
-
-btn4 =  Button(rf,padx=16,pady=16,bd = 8,bg = 'light pink',font = ('arial',20,'bold'), text = "4", command = lambda:btnClick(4)).grid(row=3,column=0)
-btn5 =  Button(rf,padx=16,pady=16,bd = 8,bg = 'light pink',font = ('arial',20,'bold'), text = "5", command = lambda:btnClick(5)).grid(row=3,column=1)
-btn6 =  Button(rf,padx=16,pady=16,bd = 8,bg = 'light pink',font = ('arial',20,'bold'), text = "6", command = lambda:btnClick(6)).grid(row=3,column=2)
-Subtraction =  Button(rf,padx=16,pady=16,bd = 8,bg = 'light pink',font = ('arial',20,'bold'), text = "-", command = lambda:btnClick("-")).grid(row=3,column=3)
-
-btn7 =  Button(rf,padx=16,pady=16,bd = 8,bg = 'light pink',font = ('arial',20,'bold'), text = "7", command = lambda:btnClick(7)).grid(row=4,column=0)
-btn8 =  Button(rf,padx=16,pady=16,bd = 8,bg = 'light pink',font = ('arial',20,'bold'), text = "8", command = lambda:btnClick(8)).grid(row=4,column=1)
-btn9 =  Button(rf,padx=16,pady=16,bd = 8,bg = 'light pink',font = ('arial',20,'bold'), text = "9", command = lambda:btnClick(9)).grid(row=4,column=2)
-Multiplication =  Button(rf,padx=16,pady=16,bd = 8,bg = 'light pink',font = ('arial',20,'bold'), text = "", command = lambda:btnClick("")).grid(row=4,column=3)
-
-btn0 =  Button(rf,padx=16,pady=16,bd = 8,bg = 'light pink',font = ('arial',20,'bold'), text = "0", command = lambda:btnClick(0)).grid(row=5,column=0)
-btnClear =  Button(rf,padx=16,pady=16,bd = 8,bg = 'light pink',font = ('arial',20,'bold'), text = "C", command = btnClear).grid(row=5,column=1)
-btnEquals =  Button(rf,padx=16,pady=16,bd = 8,bg = 'light pink',font = ('arial',20,'bold'), text = "=", command = btnEquals).grid(row=5,column=2)
-btnDivision =  Button(rf,padx=16,pady=16,bd = 8,bg = 'light pink',font = ('arial',20,'bold'), text = "/", command = lambda:btnClick("/")).grid(row=5,column=3)
-#btn7 =  Button(rf,padx=16,pady=16,bd = 8,bg = 'light pink',font = ('arial',20,'bold'), text = "7", command = lambda:btnClick(7)).grid(row=4,column=0)
 
 #--------------------------------------Resturant Information  1------------------------------------------------------------
 
 rand = StringVar()
-lblReference = Label(lf,font = ('arial','16','bold'),text="Reference",bd = 16, anchor = 'w')
+"""lblReference = Label(lf,font = ('arial','16','bold'),text="Reference",bd = 16, anchor = 'w')
 lblReference.grid(row=0,column=0)
 txtReference = Entry(lf,font = ('arial','16','bold'),textvariable = rand,bd =10,insertwidth=4,bg ='pink',justify='right')
-txtReference.grid(row=0,column=1)
+txtReference.grid(row=0,column=1)"""
 
 Fries = StringVar()
 Fries.set("0")
 lblFries = Label(lf,font = ('arial','16','bold'),text="Large Fries",bd = 16, anchor = 'w')
 lblFries.grid(row=1,column=0)
-txtFries= Entry(lf,font = ('arial','16','bold'),textvariable = Fries,bd =10,insertwidth=4,bg ='pink',justify='right')
+txtFries= Spinbox(lf,from_=0,textvariable = Fries,font = ('arial','16','bold'), bg='pink',justify='right',to=1000)
 txtFries.grid(row=1,column=1)
 
 Pasta = StringVar()
 Pasta.set("0")
 lblVeg = Label(lf,font = ('arial','16','bold'),text="Pasta",bd = 16, anchor = 'w')
 lblVeg.grid(row=2,column=0)
-txtVeg= Entry(lf,font = ('arial','16','bold'),textvariable = Pasta,bd =10,insertwidth=4,bg ='pink',justify='right')
+txtVeg= Spinbox(lf,from_=0,textvariable = Pasta,font = ('arial','16','bold'), bg='pink',justify='right',to=1000)
 txtVeg.grid(row=2,column=1)
+"""font = ('arial','16','bold'),insertwidth=4,,"""
 
 Sizzlers = StringVar()
 Sizzlers.set("0")
 lblChicken = Label(lf,font = ('arial','16','bold'),text="Sizzlers",bd = 16, anchor = 'w')
 lblChicken.grid(row=3,column=0)
-txtChicken= Entry(lf,font = ('arial','16','bold'),textvariable = Sizzlers,bd =10,insertwidth=4,bg ='pink',justify='right')
+txtChicken=Spinbox(lf,from_=0,textvariable = Sizzlers,font = ('arial','16','bold'), bg='pink',justify='right',to=1000)
 txtChicken.grid(row=3,column=1)
 
 Drinks = StringVar()
 Drinks.set("0")
 lblDrinks = Label(lf,font = ('arial','16','bold'),text="Drinks",bd = 16, anchor = 'w')
-lblDrinks.grid(row=4,column=0)
-txtDrinks= Entry(lf,font = ('arial','16','bold'),textvariable = Drinks,bd =10,insertwidth=4,bg ='pink',justify='right')
-txtDrinks.grid(row=4,column=1)
+lblDrinks.grid(row=3,column=2)
+txtDrinks= Spinbox(lf,from_=0,textvariable = Drinks,font = ('arial','16','bold'), bg='pink',justify='right',to=1000)
+txtDrinks.grid(row=3,column=3)
 
-Pizza = StringVar()
+"""Pizza = StringVar()
 Pizza.set("0")
 lblDrinks = Label(lf,font = ('arial','16','bold'),text="Pizza",bd = 16, anchor = 'w')
 lblDrinks.grid(row=0,column=2)
 txtDrinks= Entry(lf,font = ('arial','16','bold'),textvariable = Pizza,bd =10,insertwidth=4,bg ='pink',justify='right')
-txtDrinks.grid(row=0,column=3)
+txtDrinks.grid(row=0,column=3)"""
 
 
 Cookies = StringVar()
 Cookies.set("0")
 lblservice = Label(lf,font = ('arial','16','bold'),text="Cookies ",bd = 16, anchor = 'w')
 lblservice.grid(row=1,column=2)
-txtservice= Entry(lf,font = ('arial','16','bold'),textvariable =Cookies,bd =10,insertwidth=4,bg ='pink',justify='right')
+txtservice= Spinbox(lf,from_=0,textvariable = Cookies,font = ('arial','16','bold'), bg='pink',justify='right',to=1000)
 txtservice.grid(row=1,column=3)
 
 Tax = StringVar()
 lblTax = Label(lf,font = ('arial','16','bold'),text="Tax",bd = 16, anchor = 'w')
-lblTax.grid(row=3,column=2)
-txtTax= Entry(lf,font = ('arial','16','bold'),textvariable = Tax,bd =10,insertwidth=4,bg ='pink',justify='right')
-txtTax.grid(row=3,column=3)
+lblTax.grid(row=5,column=0)
+txtTax= Label(lf,font = ('arial','16','bold'),textvariable = Tax,bd =10)
+txtTax.grid(row=5,column=1)
 
 Cheesecake = StringVar()
 Cheesecake.set("0")
 lblservice = Label(lf,font = ('arial','16','bold'),text="Cheesecake ",bd = 16, anchor = 'w')
 lblservice.grid(row=2,column=2)
-txtservice= Entry(lf,font = ('arial','16','bold'),textvariable =Cheesecake,bd =10,insertwidth=4,bg ='pink',justify='right')
+txtservice= Spinbox(lf,from_=0,textvariable = Cheesecake,font = ('arial','16','bold'), bg='pink',justify='right',to=1000)
 txtservice.grid(row=2,column=3)
 
 TotalCost = StringVar()
-lblTotalCost = Label(lf,font = ('arial','16','bold'),text="TotalCost",bd = 16, anchor = 'w')
-lblTotalCost.grid(row=4,column=2)
-txtTotalCost= Entry(lf,font = ('arial','16','bold'),textvariable = TotalCost,bd =10,insertwidth=4,bg ='pink',justify='right')
-txtTotalCost.grid(row=4,column=3)
+lblTotalCost = Label(lf,font = ('arial','16','bold'),text="Total",bd = 16, anchor = 'w')
+lblTotalCost.grid(row=5,column=2)
+txtTotalCost= Label(lf,font = ('arial','16','bold'),textvariable = TotalCost,bd =10)
+txtTotalCost.grid(row=5,column=3)
 #--------------------------Buttons-----------------------------------------------------
-btnTotal = Button(lf,padx=16,pady=8,bd=16,fg='Black',font = ('arial','16','bold'),width = 10,text = "Total",bg = 'pink', command=Ref).grid(row=7,column=1)
-btnReset = Button(lf,padx=16,pady=8,bd=16,fg='Black',font = ('arial','16','bold'),width = 10,text = "Reset",bg = 'pink', command=Reset).grid(row=7,column=2)
-btnExit= Button(lf,padx=16,pady=8,bd=16,fg='Black',font = ('arial','16','bold'),width = 10,text = "Exit",bg = 'pink', command=qExit).grid(row=7,column=3)
+btnprice=Button(lf,padx=16,pady=8, bd=10 ,fg="black",font=('ariel' ,16,'bold'),width=10, text="PRICE", bg="pink",command=price)
+btnprice.grid(row=8,column=0)
+btnTotal = Button(lf,padx=16,pady=8,bd=10,fg='Black',font = ('arial','16','bold'),width = 10,text = "Total",bg = 'pink', command=Ref).grid(row=8,column=1)
+btnReset = Button(lf,padx=16,pady=8,bd=10,fg='Black',font = ('arial','16','bold'),width = 10,text = "Reset",bg = 'pink', command=Reset).grid(row=8,column=2)
+btnExit= Button(lf,padx=16,pady=8,bd=10,fg='Black',font = ('arial','16','bold'),width = 10,text = "Exit",bg = 'pink', command=qExit).grid(row=8,column=3)
 
 
 
